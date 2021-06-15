a2a7e639cee38b587f808702ea4a0479640b4d9e
Felipe Amaral <felipe.abcosta90@gmail.com>
2021-06-14 21:05:59 -0300

    Correção do bug 4 - Acrescentada a opção 4 para sair da aplicação

--- src/TeacherHelper.java
+++ src/TeacherHelper.java
@@ -18,7 +18,7 @@ public class TeacherHelper {
 			System.out.println("1 - Calcular salário");
 			System.out.println("2 - Calcular média de notas dos alunos");
 			System.out.println("3 - Exibir a motivação do dia!");
-			System.out.println("4 - Encerrar aplicação.");
+			System.out.println("4 - Encerrar aplicação...");
 			opcao = leitor.nextInt();
 			
 			switch (opcao) {
