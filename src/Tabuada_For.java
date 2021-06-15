2cd48d77d0ec3206d728b556ce9572e2459f9e05
Felipe Amaral <felipe.abcosta90@gmail.com>
2021-06-14 21:39:36 -0300

    Tabuada com Loop For

---
+++ src/Tabuada_For.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Tabuada_For {
+
+	public static void main(String[] args) {
+		
+		Scanner leitor = new Scanner(System.in);
+		
+		int n, tabuada = 1;
+		
+		System.out.println("Tabuada com Loop For\n");
+		
+		System.out.println("Escolha o número para calcular a Tabuada:");
+		n = leitor.nextInt();
+		
+		for (int i = 1; i <=10; i++ ) 
+		{
+			tabuada = n * i;
+			System.out.println(n + " x " + i + " = " + tabuada);
+			
+		}
+		
+		leitor.close();
+
+	}
+
+}
