5cdf4bbd131fcc565289fd020be9acf60f55c45b
Felipe Amaral <felipe.abcosta90@gmail.com>
2021-06-19 09:05:34 -0300

    Aula05

--- src/Exercicio_10_Loop_For.java
+++ src/Exercicio_10_Loop_For.java
@@ -1,74 +1,76 @@
 import java.util.Scanner;
 
-public class Exercicio_10_Loop_For {
+public class Exercicio_10_Loop_For 
+{
 
-	//EX 10 – Antes da pandemia, o rodízio na cidade de São Paulo foi motivo  de  confusão  em  uma  empresa:  muitos  dos  150  colaboradores   estão   se   atrasando   e   alegando   que   o   problema  foi  o  horário  do  rodízio. 
-	
-	//Cansado  disso,  um  dos  diretores  da  empresa  solicitou  um  programa  que  receba  o  NOME   de   cada   um   dos   funcionários,   o   SETOR   desses   profissionais e o meio de transporte usado (CARRO ou ÔNIBUS). 
-	
-	//Caso o profissional use um CARRO para ir até a empresa, deve ser solicitada a digitação do último dígito numérico da placa do carro e a mensagem exibida deve ser: “Seu dia de rodízio é “ seguida do dia de rodízio. 
-	
-	//Caso o profissional use um ÔNIBUS para ir até a empresa, deve ser exibida a mensagem “rodízio não se aplica”. 
-	
-	public static void main(String[] args) {
+		//EX 10 ï¿½ Antes da pandemia, o rodï¿½zio na cidade de Sï¿½o Paulo foi motivo  de  confusï¿½o  em  uma  empresa:  muitos  dos  150  colaboradores   estï¿½o   se   atrasando   e   alegando   que   o   problema  foi  o  horï¿½rio  do  rodï¿½zio. 
 		
-		Scanner leitor = new Scanner(System.in);
+		//Cansado  disso,  um  dos  diretores  da  empresa  solicitou  um  programa  que  receba  o  NOME   de   cada   um   dos   funcionï¿½rios,   o   SETOR   desses   profissionais e o meio de transporte usado (CARRO ou ï¿½NIBUS). 
 		
-		String nome = "", setor = "";
-		int escolha, placa, func;
+		//Caso o profissional use um CARRO para ir atï¿½ a empresa, deve ser solicitada a digitaï¿½ï¿½o do ï¿½ltimo dï¿½gito numï¿½rico da placa do carro e a mensagem exibida deve ser: ï¿½Seu dia de rodï¿½zio ï¿½ ï¿½ seguida do dia de rodï¿½zio. 
 		
-		System.out.println("Informar quantidade de Funcionários existem na empresa:");
-		func = leitor.nextInt();
+		//Caso o profissional use um ï¿½NIBUS para ir atï¿½ a empresa, deve ser exibida a mensagem ï¿½rodï¿½zio nï¿½o se aplicaï¿½. 
 		
-		for (int i = 0; i < func; i++)
+		public static void main(String[] args) 
 		{
-			System.out.println("Qual seu nome?");
-			nome = leitor.next();
 			
-			System.out.println("Qual o seu setor?");
-			setor = leitor.next();
+			Scanner leitor = new Scanner(System.in);
 			
-			System.out.println("Informe o meio de Transporte.\n1 - Carro\n2 - Ônibus");
-			escolha = leitor.nextInt();
+			String nome = "", setor = "";
+			int escolha, placa, func;
 			
-			if (escolha == 1)
+			System.out.println("Informar quantidade de FuncionÃ¡rios existem na empresa:");
+			func = leitor.nextInt();
+			
+			for (int i = 0; i < func; i++)
 			{
-				System.out.println("Informe o final da placa do carro:");
-				placa = leitor.nextInt();
+				System.out.println("Qual seu nome?");
+				nome = leitor.next();
 				
-				if (placa == 1 || placa == 2) 
+				System.out.println("Qual o seu setor?");
+				setor = leitor.next();
+				
+				System.out.println("Informe o meio de Transporte.\n1 - Carro\n2 - Ã”nibus");
+				escolha = leitor.nextInt();
+				
+				if (escolha == 1)
 				{
-					System.out.println("Seu dia de rodízio é SEGUNDA-FEIRA");
+					System.out.println("Informe o final da placa do carro:");
+					placa = leitor.nextInt();
+					
+					if (placa == 1 || placa == 2) 
+					{
+						System.out.println(nome + " do setor de " + setor + "\nSeu dia de rodÃ­zio Ã© SEGUNDA-FEIRA");
+					}
+					else if (placa == 3 || placa == 4) 
+					{
+						System.out.println(nome + " do setor de " + setor + "\nSeu dia de rodÃ­zio Ã© TERÃ‡A-FEIRA");
+					}
+					else if (placa == 5 || placa == 6) 
+					{
+						System.out.println(nome + " do setor de " + setor + "\nSeu dia de rodÃ­zio Ã© QUARTA-FEIRA");
+					}
+					else if (placa == 7 || placa == 8) 
+					{
+						System.out.println(nome + " do setor de " + setor + "\nSeu dia de rodÃ­zio Ã© QUINTA-FEIRA");
+					}
+					else if (placa == 9 || placa == 0) 
+					{
+						System.out.println(nome + " do setor de " + setor + "\nSeu dia de rodÃ­zio Ã© SEXTA-FEIRA");
+					}
 				}
-				else if (placa == 3 || placa == 4) 
+				else if (escolha == 2)
 				{
-					System.out.println("Seu dia de rodízio é TERÇA-FEIRA");
+					System.out.println(nome + " do setor de " + setor + " vem ao trabalho de Ã”nibus. \nO RodÃ­zio nÃ£o se aplica.");
 				}
-				else if (placa == 5 || placa == 6) 
+				else
 				{
-					System.out.println("Seu dia de rodízio é QUARTA-FEIRA");
-				}
-				else if (placa == 7 || placa == 8) 
-				{
-					System.out.println("Seu dia de rodízio é QUINTA-FEIRA");
-				}
-				else if (placa == 9 || placa == 0) 
-				{
-					System.out.println("Seu dia de rodízio é SEXTA-FEIRA");
+					System.out.println("OpÃ§Ã£o InvÃ¡lida!");
 				}
 			}
-			else if (escolha == 2)
-			{
-				System.out.println("Rodízio não se aplica.");
-			}
-			else
-			{
-				System.out.println("Opção Inválida!");
-			}
+			
+			leitor.close();
+			
 		}
-		
-		leitor.close();
-		
-	}
 
-}
+	}
